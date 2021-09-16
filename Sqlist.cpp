7654e5c1a3c984a2eedb7b4b5b59f833c59ea51d
jameskerry651 <jameskerry651@gmail.com>
2021-09-16 15:44:42 +0800

    更新了顺序表的合并和修改了一些小错误

--- Sqlist.cpp
+++ Sqlist.cpp
@@ -9,7 +9,7 @@
 3.顺序表的打印   PrintList(&L)
 4.顺序表的插入    ListInsert(&L,i,e)
 5.顺序表的删除某个点 ListDelete(&L,i,&e)
-6.两个顺序表的合并
+6.两个顺序表的合并    void MergeList(Sqlist La,Sqlist Lb,Sqlist &Lc);
 7.根据顺序表的位置返回其数值 GetElem(&L,i,&e)
 8.顺序表的销毁    DestroyList(&L)
 9.顺序表的逆序    ListReverse_Sq(SqList &L)
@@ -37,21 +37,34 @@ void ClearList(Sqlist &L);
 Status DestroyList(Sqlist &L);
 Status GetElem(Sqlist &L,int pos,ElemType &e);
 Status ListReverse_Sq(Sqlist &L);
+Status MergeList(Sqlist La,Sqlist Lb,Sqlist &Lc);
+
 int main()
 {
-    Sqlist list;
-    InitList(list);
+    Sqlist list1;
+    Sqlist list2;
+    Sqlist list3;
+    InitList(list1);
+    InitList(list2);
     int i,num;
     for(i=1;i<5;i++){
         printf("请输入一个数字：\n");
         scanf("%d",&num);
-        ListInsert(list,i,num);
+        ListInsert(list1,i,num);
     }
-    //int a;
-    ListInsert(list,3,22);
-    PrintList(list);
-    ListReverse_Sq(list);
-    PrintList(list);
+
+    PrintList(list1);
+
+
+    for(i=1;i<5;i++){
+        printf("请输入一个数字：\n");
+        scanf("%d",&num);
+        ListInsert(list2,i,num);
+    }
+    PrintList(list2);
+
+    MergeList(list1,list2,list3);
+    PrintList(list3);
     return 0;
 }
 
@@ -73,10 +86,10 @@ Status ListInsert(Sqlist &L,int pos,ElemType e)
         newbase = (ElemType *)realloc(L.elem,(L.listsize+LISTINCREMENT)*sizeof(ElemType));
         if(!newbase)return ERROR;
         L.elem = newbase;
-
+        L.listsize = L.listsize + LISTINCREMENT;
     }
 
-    L.listsize = L.listsize + LISTINCREMENT;
+
     int i = L.length;
     while(i>=pos){
         *(L.elem+ i) = *(L.elem + i-1);
@@ -138,6 +151,38 @@ Status ListReverse_Sq(Sqlist &L){
 
     return OK;
 
+}
+// 合并后非递减排列
+Status MergeList(Sqlist La,Sqlist Lb,Sqlist &Lc){
+    ElemType *pa = La.elem;
+    ElemType *pb = Lb.elem;
+    Lc.length = La.length + Lb.length;
+    Lc.listsize = Lc.length;
+
+    Lc.elem = (ElemType *)malloc(Lc.listsize*sizeof(ElemType));
+    if(!Lc.elem) return ERROR;
+    ElemType *pc = Lc.elem;
+
+    ElemType *pa_last = La.elem + La.length -1;
+    ElemType *pb_last = Lb.elem + Lb.length -1;
+    while(pa<=pa_last && pb<=pb_last){
+        if(*pa<=*pb){
+            *pc = *pa;
+            ++ pc;
+            ++ pa;
+        }else{
+            *pc = *pb;
+            ++ pc;
+            ++ pb;
+        }
+    }
+
+    while(pa<=pa_last) *pc++ = *pa++;    // *(pc++)  优先级相同，从右至左 pc++ 先取值再加，所以呈现的效果和上面的循环一样
+    while(pb<=pb_last) *pc++ = *pb++;
+
+
+
+    return OK;
 }
 
 
