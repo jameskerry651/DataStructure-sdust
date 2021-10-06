4b06790c149dd810a4aa88857027fb991d2e71ee
jameskerry651 <jameskerrt651@gmail.com>
2021-10-06 15:45:43 +0800

    完善了栈的一些函数

--- Stack.cpp
+++ Stack.cpp
@@ -3,9 +3,9 @@
 
 /**
 栈相关的代码,需要完成的任务有
-1.栈的初始化
-2.入栈 Push
-3.出栈 Pop
+1.栈的初始化   Init_Stack()
+2.入栈            Push()
+3.出栈            Pop()
 4.打印栈的内容
 5.获取栈的长度
 6.清空栈的内容
@@ -14,6 +14,7 @@
 #define STACK_INCRESEMENT 10
 #define OK 1
 #define ERROR -1
+
 
 typedef int ElemType;
 typedef int Status;
@@ -24,17 +25,49 @@ typedef struct {
 }SqStack;
 
 // 函数的声明
-Status Init_Stack(SqStack &S){
-    S.base = (ElemType *)malloc(STACK_INIT_SIZE * sizeof(SqStack));
-    if(!S.base) return ERROR;
-    S.top = S.base;
-    S.stacksize = STACK_INIT_SIZE;
-    return OK;
-}
-
+Status Init_Stack(SqStack &S);
+Status Pop(SqStack &S,ElemType &e);
+Status Push(SqStack &S,ElemType data);
+// 主函数
 int main(){
     SqStack S;
 
 
     return 0;
 }
+
+
+//函数的定义
+Status Init_Stack(SqStack &S){
+    S.base = (ElemType *)malloc(STACK_INIT_SIZE * sizeof(SElemType));
+    if(!S.base) return ERROR;
+    S.top = S.base;
+    S.stacksize = STACK_INIT_SIZE;
+    return OK;
+}
+
+Status Pop(SqStack &S,ElemType &e)
+{
+
+    if(S->top==S->base)return ERROR;
+    e = *--S->top;
+    return OK;
+
+}
+
+Status Push(SqStack &S,ElemType data)
+{
+    if(S.top-S.base>=S.stacksize){
+        ElemType *newbase = (ElemType *)realloc(S.base,(S.stacksize+STACK_INCRESEMENT)*sizeof(ElemType));
+        if(!newbase)return ERROR;
+        S.base = newbase;
+        S.top = S.base + S.stacksize;
+        S.stacksize = S.stacksize + STACK_INCRESEMENT;
+    }
+    *S.top = data;
+    S.top ++;
+    return OK;
+
+}
+
+
